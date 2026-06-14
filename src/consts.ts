252736cfeb5d8d476d26844bc82df38c72122513
Adrian Palma <adrianpalmasantana@gmail.com>
2026-06-13 20:50:08 -0500

    [feat] titulo y descripcion meta para seo del home

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+// Colocar cualquier dato global aquí, como el título del sitio, la descripción, etc.
+
+export const SITE_TITLE = 'Ecotec Flora Médica';
+export const SITE_DESCRIPTION = 'Un sitio dedicado a la flora médica de Ecotec.';
