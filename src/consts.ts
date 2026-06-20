04b6ea3c97420ffc36c8c69c0a890a8ab0f35b4a
Adrian Palma <adrianpalmasantana@gmail.com>
2026-06-20 18:57:40 -0500

    [feat] avance del nuevo layout para home

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,5 @@
 // Colocar cualquier dato global aquí, como el título del sitio, la descripción, etc.
 
+//home page
 export const SITE_TITLE = 'Ecotec Flora Médica';
-export const SITE_DESCRIPTION = 'Un sitio dedicado a la flora médica de Ecotec.';
+export const SITE_DESCRIPTION = 'Un compendio riguroso dedicado a la preservación del conocimiento botánico y la investigación fitoquímica. Explore la taxonomía de especies medicinales bajo un marco de validación empírica y respeto cultural.';
